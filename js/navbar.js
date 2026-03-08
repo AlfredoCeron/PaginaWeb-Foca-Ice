ae6922f3ef8e9edd19536fb59a18e86ba02abefb
AlfredoCeron <lalito45_@hotmail.com>
2026-03-08 04:27:49 -0600

    fix(navbar2.css): Boton de navbar regresa a estado original al dar clic en él y fuera de él

--- js/navbar.js
+++ js/navbar.js
@@ -1,6 +1,24 @@
 const botonNavbar = document.querySelector(".botonNavBarPhone");
 const headerDer = document.querySelector(".headerDer");
 
-botonNavbar.addEventListener("click", () => {
-  headerDer.classList.toggle("active");
-});
+if (botonNavbar && headerDer) {
+  botonNavbar.addEventListener("click", () => {
+    headerDer.classList.toggle("active");
+    botonNavbar.classList.toggle(
+      "active",
+      headerDer.classList.contains("active"),
+    );
+  });
+
+  //Cerrar menú navegacion al hacer clic fuera de él
+  // Youtube: @cristianbernaldev - MenuDesplegableConHTMLcssJSmenos1Minutos
+  document.addEventListener("click", (event) => {
+    const clickDentroMenu = headerDer.contains(event.target);
+    const clickEnBoton = botonNavbar.contains(event.target);
+    // Si el clic no es dentro del menú ni en el botón, cerrar el menú
+    if (!clickDentroMenu && !clickEnBoton) {
+      headerDer.classList.remove("active");
+      botonNavbar.classList.remove("active"); // boton vuelva a estado original (lineas H)
+    }
+  });
+}
